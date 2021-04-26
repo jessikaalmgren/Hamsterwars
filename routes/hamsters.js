2cd797d59e754e38e76409c32dc46300a9e942c1
JessikaAlmgren <jessika.almgren.wennerlund@iths.se>
2021-04-26 16:36:16 +0200

    Gjort en funktion för PUT

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -47,10 +47,34 @@ router.post('/', async (req, res) => {
 	}
 
 	const docRef = await db.collection('hamsters').add(object)
-	res.send(docRef)
+	res.send(docRef.id)
+	//Har inte lyckats få med hela objektet utan endast id. 
 
 })
 
+//Vi kan kontrollera om id:et finns i databasen. Det behöver jag lägga till i denna. Denna kod kontrollerar inte och lägger isåfall till ett nytt doc i databasen.
+router.put('/:id', async (req, res) =>{
+	const object = req.body
+	const id = req.params.id
+//Kan lägga till flera grejer som begränsar vad man ska kunna ändra och inte. Kan också göra en funktion utanför detta som bestämmer dessa regler och beslut. Just nu behöver det vara ett objekt och ett namn för att man ska kunna ändra datan med put. 
+	if(!object || !object.name){
+		res.sendStatus(400)
+		return
+	}
+
+	const docRef = db.collection('hamsters').doc(id)
+	await docRef.set(object, {merge: true})
+	res.sendStatus(200)
+})
+
+// function isHamstersObject(maybeObject){
+// 	if(!maybeObject || !maybeObject.name || !maybeObject.age){
+// 		return false
+// 	}
+// 	else if(!maybeObject.name || !maybeObject.age){
+// 		return false
+// 	}
+// }
 
 
 module.exports = router
