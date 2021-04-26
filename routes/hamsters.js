ef320da9e7b95942f290638a7c4970be28e62a10
JessikaAlmgren <jessika.almgren.wennerlund@iths.se>
2021-04-26 14:56:03 +0200

    Hämtar alla hamstrar

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -6,10 +6,27 @@ const router = express.Router()
 
 
 //REST API
-router.get('/', (req, res)=>{
-	console.log('/tools REST API')
-	res.send('/tools REST API')
+router.get('/', async (req, res)=>{
+	//console.log('/tools REST API')
+	//res.send('/tools REST API')
+
+	const hamstersRef = db.collection('hamsters')
+	const snapshot = await hamstersRef.get()
+
+	if (snapshot.empty) {
+		res.send([])
+		return
+	}
+	let items = []
+	snapshot.forEach( doc => {
+		const data = doc.data()
+		data.id = doc.id
+		items.push(data)
+	})
+
+	res.send(items)
 })
+
 
 
 
